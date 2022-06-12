122b26f5cedd4384879025ca513c797aa0c0e6c5
Moee21 <mohamed_sheik@outlook.com>
2022-06-12 01:47:50 +0000

    Add Questions and its reference to js

---
+++ assets/js/script..js
@@ -0,0 +1,50 @@
+
+// questions from https://www.anadventurousworld.com/countries-quiz-questions/
+// Questions will be asked 
+let quizQuestions = [
+  {  question : "What country has the longest coastline in the world?",
+    answer : {
+        a: "USA",
+        b: "China",
+        c: "Canada"
+
+    },
+    correctAnswer: "c"
+  },
+  {
+      question: "What country is Beirut the capital of?",
+      answer: {
+          a: "Syria",
+          b: "Lebanon",
+          c: "Jordan",
+    },
+    correctAnswer: "b"  
+  },
+  {
+      question: " In which country would you find the currency ‘baht’?",
+      answer: {
+          a: "Malaysia",
+          b: "Tailand",
+          c: "Singapore",
+    },
+    correctAnswer: "b"
+  },
+  {
+      question: "What African country has the largest population?",
+      answer: {
+          a: "Eygpt",
+          b: "Ethiopia",
+          c:"Nigeria",
+    },
+    correctAnswer: "c"
+  },
+  {
+      question: "What country is also the largest island in the world?",
+      answer: {
+          a: "Greenland",
+          b: "Australia",
+          c: "Cuba",
+    },
+    correctAnswer: "a"
+  }
+];
